a6799470b5c59e15e004f040be2abe6441be4556
henni saaranen <n0sahe00@students.oamk.fi>
2022-10-16 19:37:56 +0300

    vienti repoon

---
+++ components/Gameboard.js
@@ -0,0 +1,203 @@
+import React, { useEffect, useState } from "react";
+import { Text, View, Pressable } from 'react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { Grid, Row } from "react-native-easy-grid";
+import styles from '../style/style';
+//import { useFonts } from 'expo-font';
+
+let board = [];
+const NBR_OF_DICES = 5;
+const NBR_OF_THROWS = 3;
+const MAX_SPOT = 6;
+const BONUS_POINTS_LIMIT = 63
+
+export default function Gameboard() {
+
+    const [nbrOfThrowsLeft, setNbrOfThrowsLeft] = useState(NBR_OF_THROWS);
+    const [status, setStatus] = useState('Throw dices');
+    const [selectedDices, setSelectedDices] =
+        useState(new Array(NBR_OF_DICES).fill(false));
+    const [diceSpots, setDiceSpots] = useState(new Array(NBR_OF_DICES).fill(0)); 
+    const [selectedDicePoints, setSelectedDicePoints] = 
+        useState(new Array(6).fill(false));
+    const [dicePointsTotal, setDicePointsTotal] = useState(new Array(6).fill(0));    
+
+    /* const [loaded] = useFonts({
+        'DancingScript' : require('./assets/fonts/DancingScript-VariableFont_wght.ttf') 
+    });
+    if (!loaded) {
+        return null;
+    }  */
+
+
+    const row = [];
+    for (let i = 0; i < NBR_OF_DICES; i++) {
+        row.push(
+            <Pressable
+                key={"row" + i}
+                onPress={() => selectDice(i)}>
+            <MaterialCommunityIcons
+                name={board[i]}
+                key={"row" + i}
+                size={50}
+                color={getDiceColor(i)}>
+            </MaterialCommunityIcons>
+            </Pressable>
+        );
+    }
+
+    const row2 = [];
+    for (let s = 1; s <= MAX_SPOT; s++) {
+        row2.push(
+            <Pressable                    
+              key={"row2" + s}                   
+              onPress={() => selectDicePoints(s)}> 
+            <MaterialCommunityIcons
+                name={'numeric-' + s + '-circle'}
+                key={"row2" + s}
+                size={50} 
+                color={getDicePointColor(s)} > 
+            </MaterialCommunityIcons>
+            </Pressable> 
+        );         
+    }  
+    
+    /* const row3 = [];
+        row3.push(
+            setDicePointsTotal(dicePointsTotal)
+        ); */
+    
+
+    useEffect(() => {
+        checkWinner();
+        if (nbrOfThrowsLeft === NBR_OF_THROWS) {
+            setStatus('Game has not started');
+        }
+        if (nbrOfThrowsLeft < 0) {
+            setNbrOfThrowsLeft(NBR_OF_THROWS-1);
+        }
+    }, [nbrOfThrowsLeft])
+
+    function throwDices() {
+        for (let i = 0; i < NBR_OF_DICES; i++) {
+            if (!selectedDices[i]) {
+                let randomNumber = Math.floor(Math.random() * 6 + 1);
+                board[i] = 'dice-' + randomNumber;
+            }
+            
+        }
+        setNbrOfThrowsLeft(nbrOfThrowsLeft-1); 
+        
+        /* if (nbrOfThrowsLeft > 0) {
+            !setSelectedDicePoints(false)
+            setStatus('Throw 3 times before setting points.');
+        }
+        else if (nbrOfThrowsLeft === 0 && selectedDicePoints(false)) {
+            !throwDices
+            setStatus('Select your points before next throw.')
+        }
+        else if (nbrOfThrowsLeft === 0 && selectedDicePoints(true)) {
+            setSelectedDices(new Array(NBR_OF_DICES).fill(false));
+        } */
+    }
+
+    function getDiceColor(i) {
+        if (board.every((val, i, arr) => val === arr[0])) {
+            return "#73CED6";
+        }
+        else {
+            return selectedDices[i] ? "black" : "#ba89eb";
+        }
+    }
+
+    function getDicePointColor(s) {
+        if (board.every((val, s, arr) => val === arr[0])) {
+            return "#73CED6";
+        }
+        else {
+            return selectedDicePoints[s] ? "black" : "#ba89eb";
+        }    
+    } 
+
+    function selectDice(i) {
+        let dices = [...selectedDices];
+        dices[i] = selectedDices[i] ? false : true;
+        setSelectedDices(dices);
+         /* if (selectedDices[i] === true ) {
+            const sum = selectedDices[i].reduce((total, x) => total + (x == true), 0);
+            let count = 0;
+            for (let i = 0; i < selectedDices; ++i) {
+                if (selectedDices[i] == true)
+                count++;
+            } 
+            return sum; 
+        } */
+    }
+
+    function selectDicePoints(s) {
+        let points = [...selectedDicePoints];
+        points[s] = selectedDicePoints[s] ? false : true;
+        setSelectedDicePoints(points);
+        /*if (selectedDicePoints[s] === false) {
+            setStatus('Select points.');
+             let diceSpots = s;
+            let dicePointsTotal = diceSpots * sum;
+            setDicePointsTotal(new Array(6).fill(dicePointsTotal)); 
+        } */
+    }
+    
+
+    /* function calculate() {
+        //dicePointsTotal.push(dicesSum * pointsSum);
+        let sum = points * dices;
+        diceSpots += sum; 
+        setDiceSpots(diceSpots); 
+        setDicePointsTotal(dicePointsTotal);
+    }  */
+    
+
+    function checkWinner() {
+        if (board.every((val, i, arr) => val === arr[0]) && nbrOfThrowsLeft >= 0) {
+            setStatus('All same dices, select your points.');
+            setSelectedDices(new Array(NBR_OF_DICES).fill(false));
+        }
+        else if (board.every((val, s, arr) => val > arr[0]) && nbrOfThrowsLeft === 0) {
+            setStatus('Game over.');
+            setSelectedDicePoints(new Array(6).fill(false));
+            setSelectedDices(new Array(NBR_OF_DICES).fill(false));
+        }
+        if (nbrOfThrowsLeft > 0) {
+            !setSelectedDicePoints(false)
+            setStatus('Throw 3 times before setting points.');
+            
+        }
+        /* else if (nbrOfThrowsLeft === 0 && selectedDicePoints(false)) {
+            !throwDices
+            setStatus('Select your points before next throw.')
+        }
+        else if (nbrOfThrowsLeft === 0 && selectedDicePoints(true)) {
+            setSelectedDices(new Array(NBR_OF_DICES).fill(false));
+        } */
+        else 
+            setStatus('Select and throw dices again');
+    }
+
+    return(
+        <View style={styles.gameboard}>
+            <View style={styles.flex}>{row}</View>
+            <Text style={styles.gameinfo}>THROWS LEFT: {nbrOfThrowsLeft}</Text>
+            <Text style={styles.gameinfo}>{status}</Text>
+            <Pressable style={styles.button}
+             onPress={() => throwDices()}>
+                <Text style={styles.buttonText}>Throw dices</Text>
+            </Pressable>
+            <Text style={styles.gameinfo}>TOTAL: {}</Text>
+            <Text style={styles.text}>You are {BONUS_POINTS_LIMIT} points away from bonus. </Text>
+        <Grid>    
+            <Row><Text style={styles.numbers}>{dicePointsTotal}</Text></Row>
+        </Grid>
+            <View style={styles.flex}>{row2}</View>
+        </View>
+
+    )
+}
---
+++ style/style.js
@@ -0,0 +1,74 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#cccccc'
+  },
+  header: {
+    marginTop: 30,
+    backgroundColor: '#ba89eb',
+    flexDirection: 'row'
+  },
+  footer: {
+    backgroundColor: '#ba89eb',
+    flexDirection: 'row',
+  },
+  title: {
+    color: '#000',
+    //fontFamily: 'DancingScript',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 23,
+    textAlign: 'center',
+    margin: 10,
+  },
+  author: {
+    color: '#000',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 10,
+  },
+  gameboard: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  gameinfo: {
+    backgroundColor: '#fff',
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  numbers: {
+    fontSize: 50,
+    flexDirection: "row",
+  },
+  text: {
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  flex: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  button: {
+    margin: 30,
+    flexDirection: "row",
+    padding: 10,
+    backgroundColor: "#73CED6",
+    width: 150,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color:"#2B2B52",
+    fontSize: 20
+  }
+});
